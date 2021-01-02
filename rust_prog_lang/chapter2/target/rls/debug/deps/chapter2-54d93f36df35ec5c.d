3a3d2a6da91bab364326c4b8c6be1c9523cd17e9
lbali <juan.lucas.bali@gmail.com>
2021-01-02 14:18:32 -0300

    Removing target

--- rust_prog_lang/chapter2/target/rls/debug/deps/chapter2-54d93f36df35ec5c.d
+++
@@ -1,5 +0,0 @@
-/home/lbali/proyectos/Gits/rust_lessons/rust_prog_lang/chapter2/target/rls/debug/deps/chapter2-54d93f36df35ec5c.rmeta: src/main.rs
-
-/home/lbali/proyectos/Gits/rust_lessons/rust_prog_lang/chapter2/target/rls/debug/deps/chapter2-54d93f36df35ec5c.d: src/main.rs
-
-src/main.rs:
